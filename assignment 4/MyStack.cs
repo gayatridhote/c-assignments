b909c3915a99e1dfed29abcd8cbbe3318038db82
gayatridhote <72754354+gayatridhote@users.noreply.github.com>
2023-01-02 11:34:17 +0530

    Add files via upload

---
+++ assignment 4/MyStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment_3b_and_4
+{  
+    public class StackException :Exception
+    {
+        public StackException(String message):base(message)
+        {
+
+        }
+    }
+    public class MyStack : ICloneable
+    {
+        private int[] a = new int[10];
+        private int topPosition = 0;
+        private int size;
+        public object Clone()
+        {
+            MyStack myStack = new MyStack {
+                a = this.a,
+                topPosition=this.topPosition,
+                size=this.size
+            };
+            return myStack;
+        }
+        public void push(int i)
+        {
+            try
+            {
+                if(topPosition==a.Length)
+                {
+                    throw new StackException("Fullstack Exception");
+                }
+                a[++topPosition] = i;
+                Console.WriteLine($"{i} is pushed onto the stack");
+            }
+            catch(StackException st)
+            {
+                Console.WriteLine(st);
+            }
+        }
+        public void pop()
+        {
+           try
+            {
+                if(topPosition==0)
+                {
+                    throw new StackException("Emptystack Exception");
+                }
+                a[topPosition--] = 0;
+                Console.WriteLine($"{a[topPosition]} is pooped ");
+
+            }
+            catch(StackException st)
+           {
+                Console.WriteLine(st);
+           }
+        }
+        public void display()
+        {
+            foreach(int i in a)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ assignment 4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace assignment_3b_and_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            MyStack myStack = new MyStack();
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Enter the operation\n1.Insert\n2.Extract\n3.Display\n4.Clone");
+                    i = Convert.ToInt32(Console.ReadLine());
+                    switch (i)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter new element to insert");
+                            int number = Convert.ToInt32(Console.ReadLine());
+                            myStack.push(number);
+                            break;
+                        case 2:
+                            myStack.pop();
+                            break;
+                        case 3:
+                            myStack.display();
+                            break;
+                        case 4:
+                            MyStack copyMyStack = myStack.Clone() as MyStack;
+                            copyMyStack.display();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option selected.");
+                            break;
+                    }
+                } while (i <= 4);
+            }
+            catch (StackException st)
+            {
+                Console.WriteLine(st.Message);
+            }
+        }
+    }
+}
